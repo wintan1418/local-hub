861d8d8cc048306fa9403b1b5950ba23cc422253
wintan1418 <wintan1418@gmail.com>
2025-09-01 18:20:48 -0500

    Precompile

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,10 @@
 //= link application.css
 //= link application.js
 //= link controllers/application.js
+//= link controllers/booking_calendar_controller.js
+//= link controllers/dismissable_controller.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link controllers/mobile_menu_controller.js
+//= link controllers/smooth_scroll_controller.js
+//= link controllers/dropdown_controller.js
