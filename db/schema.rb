7a9b2739254da95371a996f6e3c16e191412a720
wintan1418 <wintan1418@gmail.com>
2025-09-01 21:05:39 -0500

    Update schema with unconfirmed_email field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_02_015823) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_02_020006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -301,6 +301,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_02_015823) do
     t.datetime "confirmed_at"
     t.string "confirmation_token"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["admin_role"], name: "index_users_on_admin_role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
