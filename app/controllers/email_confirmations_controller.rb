270b1dd2e99691ba6b86148be8ceac8b871514b1
wintan1418 <wintan1418@gmail.com>
2025-09-01 20:41:33 -0500

    Create email confirmations controller

---
+++ app/controllers/email_confirmations_controller.rb
@@ -0,0 +1,30 @@
+class EmailConfirmationsController < ApplicationController
+  before_action :find_user_by_token, only: [:show]
+
+  def show
+    if @user&.email_confirmation_token == params[:token]
+      @user.confirm_email!
+      @user.send_welcome_email
+      
+      sign_in(@user) unless user_signed_in?
+      redirect_to root_path, notice: 'Your email has been confirmed successfully! Welcome to LocalServiceHub.'
+    else
+      redirect_to root_path, alert: 'Invalid or expired confirmation link.'
+    end
+  end
+
+  def create
+    if user_signed_in?
+      current_user.send_confirmation_email
+      render json: { message: 'Confirmation email sent!' }
+    else
+      render json: { error: 'You must be signed in.' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def find_user_by_token
+    @user = User.find_by(email_confirmation_token: params[:token]) if params[:token].present?
+  end
+end
