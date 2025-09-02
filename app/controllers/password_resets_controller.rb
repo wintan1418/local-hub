8dacbd65a3269f3a6eacd3f33047a69454d08a74
wintan1418 <wintan1418@gmail.com>
2025-09-01 20:41:38 -0500

    Create password resets controller

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,47 @@
+class PasswordResetsController < ApplicationController
+  before_action :find_user_by_token, only: [:show, :update]
+  
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    
+    if @user
+      @user.send_password_reset_email
+      redirect_to new_user_session_path, notice: 'Password reset instructions have been sent to your email.'
+    else
+      flash.now[:alert] = 'Email address not found.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    if @user && !@user.password_reset_expired?
+      # Show password reset form
+    else
+      redirect_to new_password_reset_path, alert: 'Password reset link is invalid or has expired.'
+    end
+  end
+
+  def update
+    if @user && !@user.password_reset_expired?
+      if params[:password].present? && params[:password] == params[:password_confirmation]
+        @user.reset_password!(params[:password])
+        sign_in(@user)
+        redirect_to root_path, notice: 'Your password has been reset successfully!'
+      else
+        @user.errors.add(:password, 'Password and confirmation must match and cannot be blank')
+        render :show, status: :unprocessable_entity
+      end
+    else
+      redirect_to new_password_reset_path, alert: 'Password reset link is invalid or has expired.'
+    end
+  end
+
+  private
+
+  def find_user_by_token
+    @user = User.find_by(password_reset_token: params[:token]) if params[:token].present?
+  end
+end
