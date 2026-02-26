5f1d36519491c2b7862492bbb6fb9b4b6a899e4a
Wintan <wintan1418@gmail.com>
2026-02-26 13:46:29 +0100

    fix propshaft issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
   # Full error reports are disabled.
   config.consider_all_requests_local = false
   config.hosts << ".hatchboxapp.com"
-
+  config.assets.check_precompiled_asset = false                                                                   
+   
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
 
