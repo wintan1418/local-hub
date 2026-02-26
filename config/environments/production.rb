358e957df52cbd31abd737dd8de52fec0b509e4e
Wintan <wintan1418@gmail.com>
2026-02-26 13:34:24 +0100

    update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
 
   # Full error reports are disabled.
   config.consider_all_requests_local = false
+  config.hosts << ".hatchboxapp.com"
 
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
